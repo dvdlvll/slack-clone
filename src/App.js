fd99216029a736938d9710357ea3d373d2526363
Dave Lavilla <davelavilla@gmail.com>
2022-07-13 19:45:17 +0800

    added initial routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landing-page";
 
 function App() {
   return (
-    <>
-      <div className="App"></div>
-      <LandingPage />
-    </>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/channel" />
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
