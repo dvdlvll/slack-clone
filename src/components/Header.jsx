517d9412f1708647fd6c8b0719c0b68d4855e635
Dave Lavilla <davelavilla@gmail.com>
2022-07-13 20:48:04 +0800

    established basic ui for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,49 @@
 // react libraries //
 import React from "react";
 import styled from "styled-components";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 
 // styled components //
-const Container = styled.div;
+const HeaderContainer = styled.div``;
 
+const HeaderMain = styled.div`
+  display: flex;
+`;
+
+const SearchbarContainer = styled.div``;
+
+const Search = styled.div``;
+
+const UserContainer = styled.div`
+  display: flex;
+`;
+
+const Name = styled.div``;
+
+const UserImage = styled.div``;
+
+// component //
 function Header() {
   return (
-    <Container>
-      <SearchbarContainer></SearchbarContainer>
-      {/* <UserContainer></UserContainer> */}
-    </Container>
+    <HeaderContainer>
+      <HeaderMain>
+        <AccessTimeIcon />
+        <SearchbarContainer>
+          <Search>
+            <input type="text" placeholder="Search" />
+          </Search>
+        </SearchbarContainer>
+        <HelpOutlineIcon />
+      </HeaderMain>
+
+      <UserContainer>
+        <Name>Piss</Name>
+        <UserImage>
+          <img src="https://i.imgur.com/6VBx3io.png" />
+        </UserImage>
+      </UserContainer>
+    </HeaderContainer>
   );
 }
 
