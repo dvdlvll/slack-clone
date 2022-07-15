af61903ba86c9d59100d416377243042066cb94c
Dave Lavilla <davelavilla@gmail.com>
2022-07-15 19:09:03 +0800

    finalized sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,11 @@
 import React from "react";
 import styled from "styled-components";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import AddIcon from "@mui/icons-material/Add";
+
+// data //
+import { sidebarItems } from "../data/sidebar-data";
+import { channelItems } from "../data/channel-data";
 
 // styled components //
 const SidebarContainer = styled.div`
@@ -38,6 +43,65 @@ const NewMessage = styled.div`
   cursor: pointer;
 `;
 
+const MainChannels = styled.div`
+  padding: 10px 0;
+`;
+
+const MainChannelItem = styled.div`
+  display: grid;
+  grid-template-columns: 15% auto;
+  align-items: center;
+
+  height: 30px;
+  padding: 5px 10px;
+
+  color: rgb(188, 171, 188);
+
+  cursor: pointer;
+
+  :hover {
+    background: #350d36;
+  }
+
+  transition: 0.3s ease;
+`;
+
+const ChannelsContainer = styled.div`
+  color: rgb(188, 171, 188);
+`;
+
+const NewChannelContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 20px;
+  padding: 10px 10px;
+`;
+
+const ChannelsList = styled.div`
+  padding: 10px 0;
+`;
+
+const Channel = styled.div`
+  display: grid;
+  grid-template-columns: 15% auto;
+  align-items: center;
+
+  height: 25px;
+  padding-left: 10px;
+
+  color: rgb(188, 171, 188);
+
+  cursor: pointer;
+
+  :hover {
+    background: #350d36;
+  }
+
+  transition: 0.3s ease;
+`;
+
 // component //
 function Sidebar() {
   return (
@@ -48,6 +112,31 @@ function Sidebar() {
           <AddCircleOutlineIcon />
         </NewMessage>
       </WorkspaceContainer>
+
+      <MainChannels>
+        {sidebarItems.map((item) => (
+          <MainChannelItem>
+            {item.icon}
+            {item.text}
+          </MainChannelItem>
+        ))}
+      </MainChannels>
+
+      <ChannelsContainer>
+        <NewChannelContainer>
+          <div>Channels</div>
+          <AddIcon />
+        </NewChannelContainer>
+
+        <ChannelsList>
+          {channelItems.map((item) => (
+            <Channel>
+              {item.icon}
+              {item.text}
+            </Channel>
+          ))}
+        </ChannelsList>
+      </ChannelsContainer>
     </SidebarContainer>
   );
 }
