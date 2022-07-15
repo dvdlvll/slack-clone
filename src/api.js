58ba1e17f4df26269a514d3994d613cf6a24d26a
Dave Lavilla <davelavilla@gmail.com>
2022-07-15 21:04:13 +0800

    preliminary api code

---
+++ src/api.js
@@ -0,0 +1,105 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://slackapi.avionschool.com/api/v1/";
+
+//axios.create
+
+export const userSessionAPI = (data) => {
+  return axios({
+    method: "post",
+    url: data.url,
+    data: {
+      email: data.email,
+      password: data.password,
+      password_confirmation: data.password_confirmation,
+    },
+  });
+};
+
+export const getListsAPI = (data) => {
+  return axios({
+    method: "GET",
+    url: data.url,
+    headers: {
+      "access-token": data["access-token"],
+      client: data.client,
+      expiry: data.expiry,
+      uid: data.uid,
+    },
+  })
+    .then((res) => res)
+    .catch((err) => err);
+};
+
+export const getMessagesAPI = (data) => {
+  return axios({
+    method: "GET",
+    url: `messages?receiver_id=${data.user_id}&receiver_class=${data.receiver_class}`,
+    params: {
+      receiver_id: data.user_id,
+      receiver_class: data.receiver_class,
+    },
+    headers: {
+      "access-token": data["access-token"],
+      client: data.client,
+      expiry: data.expiry,
+      uid: data.uid,
+    },
+  })
+    .then((res) => res)
+    .catch((err) => err);
+};
+
+export const createChannelAPI = (data) => {
+  return axios({
+    method: "POST",
+    url: "channels",
+    data: {
+      name: data.name,
+      user_ids: data.user_ids,
+    },
+    headers: {
+      "access-token": data["access-token"],
+      client: data.client,
+      expiry: data.expiry,
+      uid: data.uid,
+    },
+  })
+    .then((res) => res)
+    .catch((err) => err);
+};
+
+export const sendMessageAPI = (data) => {
+  return axios({
+    method: "POST",
+    url: data.url,
+    data: {
+      receiver_id: data.receiver_id,
+      receiver_class: data.receiver_class,
+      body: data.message,
+    },
+    headers: {
+      "access-token": data["access-token"],
+      client: data.client,
+      expiry: data.expiry,
+      uid: data.uid,
+    },
+  });
+};
+
+export const addUserChannelAPI = (data) => {
+  return axios({
+    method: "POST",
+    url: "channel/add_member",
+    data: {
+      id: data.channel_id,
+      member_id: data.member_id,
+    },
+    headers: {
+      "access-token": data["access-token"],
+      client: data.client,
+      expiry: data.expiry,
+      uid: data.uid,
+    },
+  });
+};
---
+++ src/data/channel-data.js
@@ -0,0 +1,20 @@
+import TagIcon from "@mui/icons-material/Tag";
+
+export const channelItems = [
+  {
+    icon: <TagIcon />,
+    text: "Channel 1",
+  },
+  {
+    icon: <TagIcon />,
+    text: "Channel 2",
+  },
+  {
+    icon: <TagIcon />,
+    text: "Channel 3",
+  },
+  {
+    icon: <TagIcon />,
+    text: "Channel 4",
+  },
+];
