53b6b00e5a0a8a29c3f7cfbf5ded5002f8955a84
Dave Lavilla <davelavilla@gmail.com>
2022-07-22 23:26:22 +0800

    adjusted styles

--- src/components/main-functionalities/navs/ChannelList.jsx
+++ src/components/main-functionalities/navs/ChannelList.jsx
@@ -3,84 +3,13 @@ import React, { useContext, useEffect } from "react";
 import { UserContext } from "../../../utils/context";
 import { NavLink } from "react-router-dom";
 
-// axios //
-import { getCall } from "../../../utils/api-calls";
-import { SEND_ENDPOINT } from "../../../utils/api-urls";
-
 // components //
 import Photo from "../../Photo";
 
 function ChannelList() {
   // context //
-  const {
-    channelList,
-    handleSetLoadData,
-    currentHeaders,
-    loadData,
-    setAllChannelMessages,
-    setShowChatInfo,
-  } = useContext(UserContext);
-
-  const getRecentChannelMsgs = () => {
-    if (channelList) {
-      if (!channelList.data.errors) {
-        // set params and headers //
-        for (let i = 0; i < channelList.data.data.length; i++) {
-          let recentChannelMsgsRequest = {
-            "access-token": currentHeaders["access-token"],
-            client: currentHeaders.client,
-            expiry: currentHeaders.expiry,
-            uid: currentHeaders.uid,
-          };
-          let recentChannelMsgsParams = {
-            receiver_id: parseInt(channelList.data.data[i].id),
-            receiver_class: "Channel",
-          };
-          let emptyObj = {};
-
-          // set success and error //
-          let onSuccessMessage = (response) => {
-            if (response.data.data.length > 0) {
-              setAllChannelMessages((allChannelMessages) => [
-                ...allChannelMessages,
-                {
-                  id: parseInt(channelList.data.data[i].id),
-                  message: response.data.data.at(-1).body,
-                  sender: response.data.data.at(-1).sender.email,
-                },
-              ]);
-            } else {
-              setAllChannelMessages((allChannelMessages) => [
-                ...allChannelMessages,
-                {
-                  id: parseInt(channelList.data.data[i].id),
-                  message: "No messages",
-                  sender: null,
-                },
-              ]);
-            }
-          };
-
-          let onError = (error) => {
-            console.log(error);
-          };
-
-          // axios call //
-          getCall(
-            `${SEND_ENDPOINT}?receiver_id=${recentChannelMsgsParams.receiver_id}&receiver_class=${recentChannelMsgsParams.receiver_class}`,
-            emptyObj,
-            recentChannelMsgsRequest,
-            onSuccessMessage,
-            onError
-          );
-        }
-      }
-    }
-  };
-
-  useEffect(() => {
-    getRecentChannelMsgs();
-  }, [loadData, currentHeaders]);
+  const { channelList, handleSetLoadData, setShowChatInfo } =
+    useContext(UserContext);
 
   if (channelList.data.errors) {
     return (
